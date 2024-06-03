ebd7b0f8e0f69cc88cdd52e02677b2da0b00b992
carlos <carlosramos352m@gmail.com>
2024-06-02 23:36:51 -0600

    Agregar archivos de proyecto.

---
+++ CarlosRamosExamenCRUD/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace CarlosRamosExamenCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxSKU = new System.Windows.Forms.TextBox();
+            this.comboBoxProveedor = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBoxCategoria = new System.Windows.Forms.ComboBox();
+            this.maskedTextBoxCosto = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBoxPrecioVenta = new System.Windows.Forms.MaskedTextBox();
+            this.numericUpDownSTOK = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.botonGrabar = new System.Windows.Forms.Button();
+            this.botonActualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSTOK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre de Producto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Proveedor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(477, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Categoria";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(122, 359);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Costo de Producto";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(477, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Precio de Venta";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(753, 359);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Disponible";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(743, 240);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(140, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Fecha de Registro";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(254, 107);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(394, 26);
+            this.textBoxNombre.TabIndex = 8;
+            // 
+            // textBoxSKU
+            // 
+            this.textBoxSKU.Location = new System.Drawing.Point(747, 107);
+            this.textBoxSKU.Name = "textBoxSKU";
+            this.textBoxSKU.Size = new System.Drawing.Size(170, 26);
+            this.textBoxSKU.TabIndex = 9;
+            // 
+            // comboBoxProveedor
+            // 
+            this.comboBoxProveedor.FormattingEnabled = true;
+            this.comboBoxProveedor.Location = new System.Drawing.Point(126, 273);
+            this.comboBoxProveedor.Name = "comboBoxProveedor";
+            this.comboBoxProveedor.Size = new System.Drawing.Size(239, 28);
+            this.comboBoxProveedor.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(743, 69);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(132, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "SKU de Producto";
+            // 
+            // comboBoxCategoria
+            // 
+            this.comboBoxCategoria.FormattingEnabled = true;
+            this.comboBoxCategoria.Location = new System.Drawing.Point(481, 273);
+            this.comboBoxCategoria.Name = "comboBoxCategoria";
+            this.comboBoxCategoria.Size = new System.Drawing.Size(213, 28);
+            this.comboBoxCategoria.TabIndex = 12;
+            // 
+            // maskedTextBoxCosto
+            // 
+            this.maskedTextBoxCosto.Location = new System.Drawing.Point(126, 404);
+            this.maskedTextBoxCosto.Name = "maskedTextBoxCosto";
+            this.maskedTextBoxCosto.Size = new System.Drawing.Size(215, 26);
+            this.maskedTextBoxCosto.TabIndex = 13;
+            // 
+            // maskedTextBoxPrecioVenta
+            // 
+            this.maskedTextBoxPrecioVenta.Location = new System.Drawing.Point(481, 404);
+            this.maskedTextBoxPrecioVenta.Name = "maskedTextBoxPrecioVenta";
+            this.maskedTextBoxPrecioVenta.Size = new System.Drawing.Size(108, 26);
+            this.maskedTextBoxPrecioVenta.TabIndex = 14;
+            // 
+            // numericUpDownSTOK
+            // 
+            this.numericUpDownSTOK.Location = new System.Drawing.Point(757, 405);
+            this.numericUpDownSTOK.Name = "numericUpDownSTOK";
+            this.numericUpDownSTOK.Size = new System.Drawing.Size(126, 26);
+            this.numericUpDownSTOK.TabIndex = 15;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(747, 275);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(400, 26);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(143, 107);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(50, 26);
+            this.textBoxID.TabIndex = 17;
+            // 
+            // botonGrabar
+            // 
+            this.botonGrabar.Location = new System.Drawing.Point(398, 542);
+            this.botonGrabar.Name = "botonGrabar";
+            this.botonGrabar.Size = new System.Drawing.Size(212, 54);
+            this.botonGrabar.TabIndex = 18;
+            this.botonGrabar.Text = "Grabar";
+            this.botonGrabar.UseVisualStyleBackColor = true;
+            // 
+            // botonActualizar
+            // 
+            this.botonActualizar.Location = new System.Drawing.Point(757, 532);
+            this.botonActualizar.Name = "botonActualizar";
+            this.botonActualizar.Size = new System.Drawing.Size(175, 46);
+            this.botonActualizar.TabIndex = 19;
+            this.botonActualizar.Text = "Actualizar";
+            this.botonActualizar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1559, 925);
+            this.Controls.Add(this.botonActualizar);
+            this.Controls.Add(this.botonGrabar);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.numericUpDownSTOK);
+            this.Controls.Add(this.maskedTextBoxPrecioVenta);
+            this.Controls.Add(this.maskedTextBoxCosto);
+            this.Controls.Add(this.comboBoxCategoria);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBoxProveedor);
+            this.Controls.Add(this.textBoxSKU);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSTOK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxSKU;
+        private System.Windows.Forms.ComboBox comboBoxProveedor;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBoxCategoria;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxCosto;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPrecioVenta;
+        private System.Windows.Forms.NumericUpDown numericUpDownSTOK;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFechaRegistro;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Button botonGrabar;
+        private System.Windows.Forms.Button botonActualizar;
+    }
+}
+
